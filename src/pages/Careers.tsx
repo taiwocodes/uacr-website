e03f6b2902aab7ea7c3f1a17d23563a2e44005e6
pineapplegoose <teniolakalaro@gmail.com>
2025-10-30 15:45:01 +0100

    update

--- src/pages/Careers.tsx
+++ src/pages/Careers.tsx
@@ -1,7 +1,18 @@
 import { Flex } from "@chakra-ui/react"
+import { useState } from "react"
 
 export const Careers = () => {
+    const filters=["All","Operations","Management","Security","Nutrition"]
+
+    const [selectedFilters,setSelectedFilters]=useState("All")
+
     return (
         <Flex>Careers</Flex>
     )
 }
+
+const vacancies = [
+    {role:"Operations Manager", category:["Operations","Management"] },
+    {role:"Security Manager", category:["Security","Management"]},
+    {role:"Pastry Chef", category:"Nutrition"}
+]
